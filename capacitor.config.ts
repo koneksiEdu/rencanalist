0b37378f1b8853ce6c55ab07dd60628b3c3ac2ea
koneksiEdu <sadataqwim@gmail.com>
2026-03-08 12:17:47 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rencanalist',
+  webDir: 'dist'
+};
+
+export default config;
